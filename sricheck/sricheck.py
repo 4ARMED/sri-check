332b808aebb9ed5627be2a5419bb04d052c6d61b
Marc Wickenden <marc@4armed.com>
2023-10-05 10:12:44 +0100

    Convert to class to make testing easier

--- sricheck/sricheck.py
+++ sricheck/sricheck.py
@@ -8,16 +8,6 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
-whitelisted_hosts = []
-
-def is_whitelisted(netloc):
-    try:
-        whitelisted_hosts.index(netloc)
-    except ValueError:
-        return False
-    else:
-        return True
-
 def generate_sha(remote_resource_tag):
     tag = remote_resource_tag['tag']
     resource_data = requests.get(remote_resource_tag['tag'][remote_resource_tag['attr']]).content
@@ -27,71 +17,93 @@ def generate_sha(remote_resource_tag):
 
     return tag
 
-def get_html(url="", browser=False, headers={}):
-    if browser:
-        from selenium import webdriver
-        from selenium.webdriver.chrome.options import Options
+class SRICheck:
+    def __init__(self, url, browser=False, headers={}, whitelisted_hosts = [], all=False):
+        self.url = url
+        self.browser = browser
+        self.headers = headers
+        self.whitelisted_hosts = whitelisted_hosts
+        self.all = all
 
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.experimental_options["prefs"] = {
-            "profile.default_content_settings": {
-                "images": 2
+    def is_whitelisted(self, netloc):
+        try:
+            self.whitelisted_hosts.index(netloc)
+        except ValueError:
+            return False
+        else:
+            return True
+
+
+    def get_html(self):
+        if self.browser:
+            from selenium import webdriver
+            from selenium.webdriver.chrome.options import Options
+
+            chrome_options = Options()
+            chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--no-sandbox")
+            chrome_options.add_argument("--disable-dev-shm-usage")
+            chrome_options.experimental_options["prefs"] = {
+                "profile.default_content_settings": {
+                    "images": 2
+                }
             }
-        }
 
-        browser = webdriver.Chrome("chromedriver", options=chrome_options)
+            browser = webdriver.Chrome("chromedriver", options=chrome_options)
 
-        def interceptor(request):
-            request.headers.update(headers)
+            def interceptor(request):
+                request.headers.update(self.headers)
 
-        browser.request_interceptor = interceptor
+            browser.request_interceptor = interceptor
 
-        browser.get(url)
-        return browser.page_source
-    else:
-        # file deepcode ignore Ssrf: The purpose of the script is to parse remote URLs from the CLI
-        return requests.get(url, headers=headers).content
+            browser.get(self.url)
+            return browser.page_source
+        else:
+            # file deepcode ignore Ssrf: The purpose of the script is to parse remote URLs from the CLI
+            return requests.get(self.url, headers=self.headers).content
 
 
-def get_remote_resource_tags(url="", browser=False, headers={}, all=False):
-    html = get_html(url=url, browser=browser, headers=headers)
-    soup = BeautifulSoup(html, 'lxml')
+    def get_remote_resource_tags(self, html):
+        soup = BeautifulSoup(html, 'lxml')
 
-    resource_tags = []
-    remote_resource_tags = []
+        resource_tags = []
+        remote_resource_tags = []
 
-    if all:
-        script_tags = [tag for tag in soup.find_all(['script'], attrs={'src':True})]
-        link_tags = [tag for tag in soup.find_all(['link'], attrs={'href':True})]
-        resource_tags.extend(script_tags)
-        resource_tags.extend(link_tags)
-    else:
-        script_tags = [tag for tag in soup.find_all(['script'], attrs={'src':True, 'integrity':None})]
-        link_tags = [tag for tag in soup.find_all(['link'], attrs={'href':True, 'integrity':None})]
-        resource_tags.extend(script_tags)
-        resource_tags.extend(link_tags)
+        if self.all:
+            script_tags = [tag for tag in soup.find_all(['script'], attrs={'src':True})]
+            link_tags = [tag for tag in soup.find_all(['link'], attrs={'href':True})]
+            resource_tags.extend(script_tags)
+            resource_tags.extend(link_tags)
+        else:
+            script_tags = [tag for tag in soup.find_all(['script'], attrs={'src':True, 'integrity':None})]
+            link_tags = [tag for tag in soup.find_all(['link'], attrs={'href':True, 'integrity':None})]
+            resource_tags.extend(script_tags)
+            resource_tags.extend(link_tags)
 
-    if len(resource_tags) > 0:
-        parsed_source_url = urlparse(url)
+        if len(resource_tags) > 0:
+            parsed_source_url = urlparse(self.url)
 
-        for resource_tag in resource_tags:
-            attribute = ""
-            for potential_attribute in ['src', 'href']:
-                if potential_attribute in resource_tag.attrs:
-                    attribute = potential_attribute
+            for resource_tag in resource_tags:
+                attribute = ""
+                for potential_attribute in ['src', 'href']:
+                    if potential_attribute in resource_tag.attrs:
+                        attribute = potential_attribute
 
-            if re.search('^//', resource_tag[attribute]):
-                resource_tag[attribute] = parsed_source_url.scheme + ':' + resource_tag[attribute]
+                if re.search('^//', resource_tag[attribute]):
+                    resource_tag[attribute] = parsed_source_url.scheme + ':' + resource_tag[attribute]
 
-            parsed_tag = urlparse(resource_tag[attribute])
-            if parsed_tag.scheme in {'http', 'https'}:
-                if is_whitelisted(parsed_tag.netloc) is False:
-                    remote_resource_tags.append({'tag': resource_tag, 'attr': attribute})
+                parsed_tag = urlparse(resource_tag[attribute])
+                if parsed_tag.scheme in {'http', 'https'}:
+                    if self.is_whitelisted(parsed_tag.netloc) is False:
+                        remote_resource_tags.append({'tag': resource_tag, 'attr': attribute})
 
-    return remote_resource_tags
+        return remote_resource_tags
+
+    def run(self):
+        html = self.get_html()
+        remote_resource_tags = self.get_remote_resource_tags(html)
+
+        return remote_resource_tags
 
 def cli():
     parser = argparse.ArgumentParser()
@@ -110,7 +122,6 @@ def cli():
             headers[k] = v
 
     # hosts we will ignore (in netloc format), in addition to the target URL
-    global whitelisted_hosts
     whitelisted_hosts = [
         "fonts.googleapis.com", # does not use versioning so can't realistically use SRI
         "js.hs-scripts.com", # does not use versioning so can't realistically use SRI
@@ -121,7 +132,8 @@ def cli():
         for host in args.ignore:
             whitelisted_hosts.append(host)
 
-    remote_resource_tags = get_remote_resource_tags(url=args.url, browser=args.browser, headers=headers, all=args.all)
+    s = SRICheck(url=args.url, browser=args.browser, headers=headers, whitelisted_hosts=whitelisted_hosts, all=args.all)
+    remote_resource_tags = s.run()
 
     if len(remote_resource_tags) > 0:
         for remote_resource_tag in remote_resource_tags:
