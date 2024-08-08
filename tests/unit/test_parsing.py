6d41f35cae7e8da513c31f12bca7a62e91bb3ae1
Marc Wickenden <marc@4armed.com>
2024-08-08 08:35:27 +0100

    Fixes #16 - do not report links unless they conform to HTML spec for SRI

--- tests/unit/test_parsing.py
+++ tests/unit/test_parsing.py
@@ -22,3 +22,27 @@ class TestParsing(unittest.TestCase):
         html = """<html><head><script crossorigin="anonymous" integrity="sha384-qkIfm9UUNrOzzGFh3YtL/KOHBwDNjW00Iwd0LK/DAsdmiOWRUfXBRl/s1Rtn9h8/" src="https://cdn.cloudflare.com/script.js"></script></head></html>"""
         remote_resource_tags = check.get_remote_resource_tags(html)
         self.assertEqual(len(remote_resource_tags), 0)
+    
+    def test_rel_icon_returns_no_results(self):
+        check = SRICheck("https://www.4armed.com")
+        html = """<html><head><link href="https://www.example.com/favicon.png" rel="icon" type="image/png" /></head></html>"""
+        remote_resource_tags = check.get_remote_resource_tags(html)
+        self.assertEqual(len(remote_resource_tags), 0)
+    
+    def test_rel_stylesheet_returns_result(self):
+        check = SRICheck("https://www.4armed.com")
+        html = """<html><head><link href="https://www.example.com/style.css" rel="stylesheet" /></head></html>"""
+        remote_resource_tags = check.get_remote_resource_tags(html)
+        self.assertEqual(len(remote_resource_tags), 1)
+    
+    def test_rel_preload_returns_result(self):
+        check = SRICheck("https://www.4armed.com")
+        html = """<html><head><link href="https://www.example.com/style.css" rel="preload" as="stylesheet" /></head></html>"""
+        remote_resource_tags = check.get_remote_resource_tags(html)
+        self.assertEqual(len(remote_resource_tags), 1)
+    
+    def test_rel_modulepreload_returns_result(self):
+        check = SRICheck("https://www.4armed.com")
+        html = """<html><head><link href="https://cdn.cloudflare.com/script.js" rel="modulepreload"></script></head></html>"""
+        remote_resource_tags = check.get_remote_resource_tags(html)
+        self.assertEqual(len(remote_resource_tags), 1)
